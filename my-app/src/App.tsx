3813717ad98e97cf0068b8df3142e9944f30b33e
bigcloud07 <dnsxo0712@gmail.com>
2026-04-30 16:54:37 +0900

    chore: emotion 세팅
    Co-authored-by: eunnn <DongEun02@users.noreply.github.com>

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
-import RegisterCard from "./pages/RegisterCard"
+import { ThemeProvider } from '@emotion/react';
+import RegisterCard from './pages/RegisterCard';
+import { theme } from './styles/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <RegisterCard />
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
