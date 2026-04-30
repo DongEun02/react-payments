3af08f57fc83a24052672f5ed3e27bd36c2f4c9b
bigcloud07 <dnsxo0712@gmail.com>
2026-04-30 21:43:19 +0900

    chore: 글로벌 스타일 App.tsx에서 설정

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,12 @@
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import RegisterCard from './pages/RegisterCard';
 import { theme } from './styles/theme';
+import { globalStyles } from './styles/globalStyles';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
       <RegisterCard />
     </ThemeProvider>
   );
