e19c8c80cdafeccf56f84e499017c46550192922
bigcloud07 <dnsxo0712@gmail.com>
2026-04-30 21:44:57 +0900

    feat: card 컴포넌트 css 추가 작업

--- my-app/src/components/Card.tsx
+++ my-app/src/components/Card.tsx
@@ -1,3 +1,6 @@
+import visa from '../assets/Visa.png';
+import master from '../assets/Mastercard.png';
+
 type CardProps = {
   cardBrand: string;
   cardNumbers: string[];
@@ -6,20 +9,67 @@ type CardProps = {
 
 export default function Card({ cardBrand, cardNumbers, cardExpiryDate }: CardProps) {
   return (
-    <div>
-      <div>
-        <div></div>
-        <div>{cardBrand}</div>
+    <div
+      css={(theme) => ({
+        width: '212px',
+        height: '132px',
+        backgroundColor: theme.colors.cardBackground,
+        boxShadow: '3px 3px 5px 0px #00000040;',
+        borderRadius: '4px',
+        padding: '8px 12px 8px 12px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '14px',
+      })}
+    >
+      <div
+        css={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div
+          css={(theme) => ({
+            backgroundColor: theme.colors.chipBackground,
+            width: '36px',
+            height: '22px',
+            border: `0.5px solid #DDCD781A`,
+            borderRadius: '4px',
+          })}
+        ></div>
+        {cardBrand === 'visa' ? (
+          <img src={visa} width={36} height={22} />
+        ) : cardBrand === 'master' ? (
+          <img src={master} width={36} height={22} />
+        ) : (
+          ''
+        )}
       </div>
-      <div>
+      <div
+        css={(theme) => ({
+          ...theme.typography.info,
+          color: theme.colors.white,
+          display: 'flex',
+          gap: '10px',
+        })}
+      >
         <span>{cardNumbers[0]}</span>
         <span>{cardNumbers[1]}</span>
-        <span>{cardNumbers[2]}</span>
-        <span>{cardNumbers[3]}</span>
+        <span css={{ letterSpacing: '0px', fontSize: '30px' }}>
+          {'·'.repeat(cardNumbers[2].length)}
+        </span>
+        <span css={{ letterSpacing: '0px', fontSize: '30px' }}>
+          {'·'.repeat(cardNumbers[3].length)}
+        </span>
       </div>
       {cardExpiryDate[0] !== '' && cardExpiryDate[1] !== '' && (
-        <span>
-          {cardExpiryDate[0]}/{cardExpiryDate[1]}
+        <span
+          css={(theme) => ({
+            ...theme.typography.info,
+            color: theme.colors.white,
+          })}
+        >
+          {cardExpiryDate[0].padStart(2, '0')}/{cardExpiryDate[1]}
         </span>
       )}
     </div>
