dcca9808223a3194c2f7c9ca71f4b4eedb36aa36
eunnn <ehddms0517@naver.com>
2026-05-01 16:22:03 +0900

    chore: vite.config.ts에서 base 경로를 상대 경로로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  base: '/payments/',
+  base: './',
   build: {
     outDir: 'docs',
   },
